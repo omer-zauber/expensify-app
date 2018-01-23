df49aee0856544522597d0a2599ea19f5b18bb1b
omer-zauber <omer.zauber@gmail.com>
2018-01-23 09:47:48 +0200

    removed initial data

--- src/app.js
+++ src/app.js
@@ -12,30 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() =>{
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({
-  description: 'Water Bill',
-  amount: 200,
-  createdAt: Date.now() - 86400000
-}));
-
-store.dispatch(addExpense({
-  description: 'Gas Bill',
-  amount: 250,
-  createdAt: Date.now()+30000
-}));
-
-store.dispatch(addExpense({
-  description: 'rent',
-  amount: 2000,
-  createdAt: 0
-}));
-
 const jsx = (
   <Provider store = {store}>
     <AppRouter />
